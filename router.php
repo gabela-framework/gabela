195ea9408c8da086cc223110773b5b2947042fba
Ntabethemba Ntshoza <bmaneza@gmail.com>
2024-03-08 18:37:04 +0200

    hdak  with '#' will be ignored, and an empty message aborts the commit.

--- router.php
+++ router.php
@@ -9,7 +9,7 @@ $extensionPath = $config["path"]["additionalPath"];
 $router = new Router();
 
 $router->get("{$extensionPath}/", "IndexController::Index")->pass('guest');
-$router->get("{$extensionPath}/login", "LoginPageController::index")->pass('guest');
+$router->get("{$extensionPath}/login", "LoginController::index")->pass('guest');
 
 $router->get("{$extensionPath}/forgot-password", "ForgotPasswordController::forgot")->pass('guest');
 $router->post("{$extensionPath}/forgot-password-submit", "ForgotPasswordSubmitController::submit")->pass('guest');
@@ -17,17 +17,17 @@ $router->get("{$extensionPath}/reset-password", "ResetPasswordController::reset"
 $router->post("{$extensionPath}/reset-password-submit", "ResetPasswordSubmitController::submit")->pass('guest');
 
 $router->get("{$extensionPath}/tasks", "TasksController::tasks")->pass('auth');
-$router->get("{$extensionPath}/tasks-create", "TasksCreateController::create")->pass('auth');
+$router->get("{$extensionPath}/tasks-create", "TasksController::create")->pass('auth');
 $router->post("{$extensionPath}/tasks-create-submit", "TasksCreateSubmitController::submit")->pass('auth');
-$router->get("{$extensionPath}/task-edit", "TasksEditController::edit")->pass('auth');
+$router->get("{$extensionPath}/task-edit", "TasksController::edit")->pass('auth');
 $router->post("{$extensionPath}/task-edit-submit", "TasksSubmitController::submit")->pass('auth');
 $router->get("{$extensionPath}/task-delete", "TasksDeleteController::delete")->pass('auth');
 
 $router->get("{$extensionPath}/users", "UsersController::users")->pass('auth');
-$router->get("{$extensionPath}/user-edit", "UsersEditController::edit")->pass('auth');
+$router->get("{$extensionPath}/user-edit", "UsersController::edit")->pass('auth');
+$router->get("{$extensionPath}/users-profile", "UsersController::profile")->pass('auth');
 $router->post("{$extensionPath}/users-edit-submit", "UsersSubmitController::submit")->pass('auth');
 $router->get("{$extensionPath}/user-delete", "UsersDeleteController::delete")->pass('auth');
-$router->get("{$extensionPath}/users-profile", "ProfileController::profile")->pass('auth');
 
 $router->post("{$extensionPath}/login-submit", "LoginController::login")->pass('guest');
 $router->get("{$extensionPath}/logout", "LogoutController::logout")->pass('auth');
